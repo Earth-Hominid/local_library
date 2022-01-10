7ff20badf3e4ae83cdac7ac0b57306f646335a0b
C <earthhominid@gmail.com>
2022-01-10 19:09:40 -0300

    updated detail pages and formatted due dates

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,3 +1,4 @@
+const bookinstance = require('../models/bookinstance');
 const BookInstance = require('../models/bookinstance');
 
 // Display list of all BookInstances.
@@ -18,7 +19,24 @@ exports.bookinstance_list = function (req, res, next) {
 
 // Display detail page for a specific BookInstance.
 exports.bookinstance_detail = (req, res) => {
-  res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+  BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec((err, bookinstance) => {
+      if (err) {
+        return next(err);
+      }
+      if (bookinstance == null) {
+        // No results.
+        const err = new Error('Book copy not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, thus render
+      res.render('bookinstance_detail', {
+        title: 'Copy: ' + bookinstance.book.title,
+        bookinstance: bookinstance,
+      });
+    });
 };
 
 // Display BookInstance create form on GET.
